88d1a12befd24a61a07e7f7e44afd12a40e1d44b
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-05-29 04:56:52 -0400

    a

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,6 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import { prisma } from '@/lib/prisma';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
+import { registerUseCase } from '../../use-cases/register';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,13 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  });
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    return reply.status(409).send();
+  }
 
   return reply.status(201).send();
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { register } from './register';
+import { register } from './controllers/register';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
