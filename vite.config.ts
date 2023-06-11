0fa923ac051fdfd90b4dd849a348e3e86402ff28
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-06-11 10:36:21 -0400

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite.config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({ plugins: [tsconfigPaths] });
+export default defineConfig({ plugins: [tsconfigPaths()] });
