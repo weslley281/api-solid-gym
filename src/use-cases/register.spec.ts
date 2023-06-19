a2aa94de490569fea9512531cdb59584cafd71f5
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-06-19 06:13:07 -0400

    a

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,16 @@
-import { expect, test } from 'vitest';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { describe, expect, it, test } from 'vitest';
+import { RegisterUseCase } from './register';
 
-test('a', () => {
-  expect(2 + 2).toBe(4);
+describe('Register Use Case', () => {
+  it('shold hash user password upon registration', async () => {
+    const prismaUserRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUserRepository);
+
+    await registerUseCase.execute({
+      name: 'Cicrano de tal',
+      email: 'cicrano@gmail.com',
+      password: '123456',
+    });
+  });
 });
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '@/repositories/users-repository';
 import { hash } from 'bcryptjs';
 import { UsersAlreadyExistisError } from './erros/user-already-exists-error';
+import type { User } from '@prisma/client';
 
 interface RegisterUseCaseRequest {
   name: string;
@@ -9,10 +10,18 @@ interface RegisterUseCaseRequest {
   password: string;
 }
 
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -21,10 +30,12 @@ export class RegisterUseCase {
       throw new UsersAlreadyExistisError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return { user };
   }
 }
