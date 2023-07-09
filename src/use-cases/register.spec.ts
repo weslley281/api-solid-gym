048ed6ef52b5446f61957445db309d8493b7c16c
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-07-09 16:52:15 -0400

    a

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -49,7 +49,7 @@ describe('Register Use Case', () => {
       password: '123456',
     });
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'Cicrano de tal',
         email,
