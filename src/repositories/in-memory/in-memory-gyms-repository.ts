aaf4631044049a4c8c02635aed4d857c5fe8f216
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-01 05:50:43 -0400

    a

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,9 +1,10 @@
 import { Gym, Prisma } from '@prisma/client';
-import { GymsRepository } from '../gyms-repository';
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository';
 import { randomUUID } from 'crypto';
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates';
 
 export class InMemoryGymsRepository implements GymsRepository {
-  public itens: Gym[] = [];
+  public items: Gym[] = [];
 
   async create(data: Prisma.GymCreateInput): Promise<Gym> {
     const gym = {
@@ -16,13 +17,13 @@ export class InMemoryGymsRepository implements GymsRepository {
       created_at: new Date(),
     };
 
-    this.itens.push(gym);
+    this.items.push(gym);
 
     return gym;
   }
 
   async findById(id: string): Promise<Gym | null> {
-    const gym = this.itens.find((item) => item.id === id);
+    const gym = this.items.find((item) => item.id === id);
 
     if (!gym) {
       return null;
@@ -32,8 +33,22 @@ export class InMemoryGymsRepository implements GymsRepository {
   }
 
   async searchMany(query: string, page: number): Promise<Gym[]> {
-    return this.itens
+    return this.items
       .filter((item) => item.title.includes(query))
       .slice((page - 1) * 20, page * 20);
+  }
+
+  async findManyNearby(params: FindManyNearbyParams): Promise<Gym[]> {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        }
+      );
+
+      return distance < 10;
+    });
   }
 }
