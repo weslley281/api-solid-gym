d07c99d4eaf7f9d63669a5940e056549f14089dd
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-02 19:30:00 -0400

    a

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,7 +1,7 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 import { ValidateCheckInUseCase } from './validate-check-in';
 import {} from 'node:test';
-import { expect, describe, it, beforeEach, afterEach } from 'vitest';
+import { expect, describe, it, beforeEach, afterEach, vi } from 'vitest';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 
 let checkInsRepository: InMemoryCheckInsRepository;
@@ -11,10 +11,12 @@ describe('Validate Check-in Use Case', () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
+
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers();
   });
 
   it('should be able to validate the check-in', async () => {
@@ -33,7 +35,28 @@ describe('Validate Check-in Use Case', () => {
 
   it('should not be able to validate an inexistent check-in', async () => {
     await expect(() =>
-      sut.execute({ checkInId: 'inexistent-check-in-id' })
+      sut.execute({
+        checkInId: 'inexistent-check-in-id',
+      })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error);
   });
 });
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,12 +1,14 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
 import { CheckIn } from '@prisma/client';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import dayjs from 'dayjs';
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error';
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string;
 }
 
-interface ValidateCheckInUseCaseResponce {
+interface ValidateCheckInUseCaseResponse {
   checkIn: CheckIn;
 }
 
@@ -15,11 +17,20 @@ export class ValidateCheckInUseCase {
 
   async execute({
     checkInId,
-  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponce> {
+  }: ValidateCheckInUseCaseRequest): Promise<ValidateCheckInUseCaseResponse> {
     const checkIn = await this.checkInsRepository.findById(checkInId);
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes'
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError();
     }
 
     checkIn.validated_at = new Date();
