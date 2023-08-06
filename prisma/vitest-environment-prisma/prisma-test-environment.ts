892d03f54ffb6253c376ff036bb60c11b4a3fa33
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-06 10:06:20 -0400

    a

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,14 @@
+import { Environment } from 'vitest';
+
+export default <Environment>{
+  name: 'prisma',
+  async setup() {
+    console.log('Executou');
+
+    return {
+      async teardown() {
+        console.log('Teardown');
+      },
+    };
+  },
+};
