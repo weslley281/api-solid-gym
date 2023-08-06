1677713fb800d7630253bec7426b5ddab7518eb3
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-06 05:50:40 -0400

    a

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,14 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function profile(request: FastifyRequest, response: FastifyReply) {
-  await request.jwtVerify();
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  console.log(request.user.sub);
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
 
-  return response.status(200).send();
+  return response
+    .status(200)
+    .send({ user: { ...user, password_hash: undefined } });
 }
