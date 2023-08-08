0e58d77530561c8e3b9cd542c7079997c49a03e8
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-08 16:42:41 -0400

    a

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,9 +1,22 @@
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest';
 
+function generateDataBaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Executou');
+    const schema = randomUUID();
+    console.log(generateDataBaseURL(schema));
 
     return {
       async teardown() {
