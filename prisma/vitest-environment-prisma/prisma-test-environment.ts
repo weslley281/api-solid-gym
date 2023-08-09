019a0107cccff1f913362b6a4c957189649db2ba
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-08 21:26:04 -0400

    a

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,10 +1,15 @@
 import 'dotenv/config';
-import { randomUUID } from 'node:crypto';
-import { Environment } from 'vitest';
 
-function generateDataBaseURL(schema: string) {
+import { randomUUID } from 'node:crypto';
+import { execSync } from 'node:child_process';
+import { Environment } from 'vitest';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL environment variable');
+    throw new Error('Please provide a DATABASE_URL environment variable.');
   }
 
   const url = new URL(process.env.DATABASE_URL);
@@ -13,15 +18,24 @@ function generateDataBaseURL(schema: string) {
 
   return url.toString();
 }
+
 export default <Environment>{
   name: 'prisma',
   async setup() {
     const schema = randomUUID();
-    console.log(generateDataBaseURL(schema));
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('npx prisma migrate deploy');
 
     return {
       async teardown() {
-        console.log('Teardown');
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
