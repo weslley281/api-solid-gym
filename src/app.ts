a39a3f227a7aedd300890ed513074f5507c7de87
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-13 11:24:49 -0400

    a

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify';
-import cookie from '@fastify/cookie';
+import fastifyCookie from '@fastify/cookie';
 import { ZodError } from 'zod';
 import { env } from './env';
 import fastifyJwt from '@fastify/jwt';
@@ -9,7 +9,12 @@ import { checkInsRoutes } from './http/controllers/check-ins/routes';
 
 export const app = fastify();
 
-app.register(fastifyJwt, { secret: env.JWT_SECRET, sign: { expiresIn: '1d' } });
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: { cookieName: 'refreshToken', signed: false },
+  sign: { expiresIn: '10m' },
+});
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
