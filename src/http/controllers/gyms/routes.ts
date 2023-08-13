bb24edf2ac0ddf4f705ac0ccc6584d5f41d5c992
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-13 05:54:07 -0400

    A

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -7,7 +7,7 @@ import { search } from './search';
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT);
 
-  app.get('/gyms/searsh', search);
+  app.get('/gyms/search', search);
   app.get('/gyms/nearby', nearby);
   app.post('/gyms', create);
 }
