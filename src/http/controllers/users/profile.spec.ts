69025bf53dcfcfd6becbc617915ca82280f3cf09
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-16 06:25:22 -0400

    a

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -1,7 +1,7 @@
-import { app } from '@/app';
-import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
 import request from 'supertest';
+import { app } from '@/app';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
 
 describe('Profile (e2e)', () => {
   beforeAll(async () => {
@@ -12,7 +12,7 @@ describe('Profile (e2e)', () => {
     await app.close();
   });
 
-  it('Shoud be able to get user profile', async () => {
+  it('should be able to get user profile', async () => {
     const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
@@ -22,7 +22,9 @@ describe('Profile (e2e)', () => {
 
     expect(profileResponse.statusCode).toEqual(200);
     expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({ email: 'fulano@gmail.com' })
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      })
     );
   });
 });
